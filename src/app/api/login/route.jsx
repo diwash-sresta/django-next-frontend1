9d57e7b9ddbf38dcfde1b88c2aeab0f42a223633
Diwash <diwashshrestha108@gmail.com>
2025-07-29 17:38:07 +0545

    httpOnly cookies from Nextjs api

---
+++ src/app/api/login/route.jsx
@@ -0,0 +1,22 @@
+"use server";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  const data = await request.json();
+  console.log(data);
+
+  // const authToken = (await cookies()).get("auth-token");
+  cookies().set({
+    name: "auth-token",
+    value: "abc",
+    httpOnly: true, // limit clinet-side js
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development",
+    maxAge: 3600,
+  });
+  return NextResponse.json(
+    { hello: "world"},
+    { status: 200 }
+  );
+}
